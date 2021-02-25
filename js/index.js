b234bd58377ed9eb3090b4fc52b4281c497c803c
Rencas1207 <rencasdag.12@gmail.com>
2021-02-24 20:02:48 -0500

    Mi primer commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// 'use strict'
+
+// const numeros = [1,2,3,4,5]
+
+// function validar(numeros) {
+//     for(let i = 0; i <= numeros.length; i++){
+//         if(i != numeros[i]){
+//             return console.log('EL NUMERO QUE FALTA ES: '+ i);
+//         }
+//     }
+// }
+
+// validar(numeros);
+
+// //Array
+
+// const numbers = [1, 2, 3, 4, 5, 6, 8];
+
+// //Function
+// function consecutiveNumbers(array){
+//     for(let i = 0; i<array.length; i++){
+//         if(array[i + 1] - array[i] !== 1){
+//             return array[i] + 1;
+//         }
+//     }
+// }
+
+// const missing = consecutiveNumbers(numbers);
+// console.log('The missing number is:', `${missing}`)
